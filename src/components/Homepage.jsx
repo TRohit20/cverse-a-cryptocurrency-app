e94e8b08a9e3369964309d0fef775f02636b9aee
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-18 21:17:37 +0530

    Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Typography,Row, Col,Statistic } from 'antd';
 
+const {Title} = Typography;
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <Title level={2} className="heading"> Global Crypto Statistics</Title>
+      <Row>
+        <Col span={12}> <Statistic title="All Cryptocurrencies" value="5"/></Col>
+        <Col span={12}> <Statistic title="All Exchnages" value="5"/></Col>
+        <Col span={12}> <Statistic title="Total Market Cap" value="5"/></Col>
+        <Col span={12}> <Statistic title="Total 24h Volume" value="5"/></Col>
+        <Col span={12}> <Statistic title="All Markets" value="5"/></Col>
+      </Row>
+    </>
   )
 }
 
