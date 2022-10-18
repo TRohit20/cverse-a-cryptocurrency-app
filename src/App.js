f6d4527ba7f689014d0e71ee979cd0317cd38ee9
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-18 20:57:46 +0530

    Footer Layout

--- src/App.js
+++ src/App.js
@@ -18,16 +18,27 @@ const app = () => {
       <div className="main">
         <Layout> 
           <div className="routes">
-            
               <Routes>
                 <Route exact path ="/" element={<Homepage/>}/>
+                <Route exact path ="/exchanges" element={<Exchanges/>}/>
+                <Route exact path ="/cryptocurrencies" element={<Cryptocurrencies/>}/>
+                <Route exact path ="/news" element={<News/>}/>
+                <Route excat path ="/cryptodetails" element={<CryptoDetails/>}/>
               </Routes>
-           
           </div>
         </Layout>
-      </div>
+      
       <div className="footer">
-
+        <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
+          CVerse <br/>
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+        </div>
       </div>
     </div>
   );
