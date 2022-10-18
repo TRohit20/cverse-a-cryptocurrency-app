ae97037451816c5502706435661bab8638ad5e40
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-18 20:43:25 +0530

    Main and Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Switch, Route, Link, Routes } from 'react-router-dom';
+import {Routes, Link, Route } from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
 
 // This is one and a traditional way to import components to page 
 // import Navbar from './components/Navbar';
 // USing the below method, we can import multiple components in a single line
-import { Navbar,Exchanges,Homepage,Cryptocurrencies,News,CryptoDetails } from './components';
+import { Navbar,Exchanges,Homepage,Cryptocurrencies,News,CryptoDetails} from './components';
 import './App.css';
 
 
@@ -18,24 +18,11 @@ const app = () => {
       <div className="main">
         <Layout> 
           <div className="routes">
-            <Switch>
-              <Route exact path="/">
-                <Homepage />
-              </Route>
-              <Route exact path="/exchanges">
-                <Exchanges />
-              </Route>
-              <Route exact path="/cryptocurrencies">
-                <Cryptocurrencies />
-              </Route>
-              {/* Below Colon means tge data is dynamic */}
-              <Route exact path="/crypto/:coinId">
-                <CryptoDetails />
-              </Route>
-              <Route exact path="/news">
-                <News />
-              </Route>
-            </Switch>
+            
+              <Routes>
+                <Route exact path ="/" element={<Homepage/>}/>
+              </Routes>
+           
           </div>
         </Layout>
       </div>
