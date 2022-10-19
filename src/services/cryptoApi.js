e685ba8833989b6f2d4bf66a6ca1e5c48ab1cffd
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-19 20:12:26 +0530

    Redux

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,19 @@
+// // This is where we write the logic to fetch the data using the API
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'a4801ad5e8mshfd261ff0afc73bfp1c15abjsn89d8fee9a4a2',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
